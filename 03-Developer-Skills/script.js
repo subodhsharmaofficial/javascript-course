81df28e271d324754c8140f881ba346f9682d15e
Subodh Sharma <81741899+subodhsharmaofficial@users.noreply.github.com>
2021-05-27 22:00:50 +0530

    Add files via upload

---
+++ 03-Developer-Skills/script.js
@@ -0,0 +1,47 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// const x = 23;
+
+// const calcAge = (birthYear) => 2037 - birthYear;
+
+// console.log(calcAge(1991))
+
+//PROBLEM:
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// const calcTempAmplitude = function(temps){
+//     let max = temps[0];
+//     let min = temps[0];
+//     for(let i=0; i<temps.length; i++){
+
+//         const curTemp = temps[i]
+//         if(typeof curTemp !== 'number') continue;
+//         if(curTemp>max) max = curTemp;
+//         if(curTemp<min) min = curTemp
+//     }
+//     console.log(max);
+//     console.log(min);
+
+//     return max-min
+// }
+
+// calcTempAmplitude([3, 7, ,23])
+// const amplitude = calcTempAmplitude(temperatures)
+// console.log(amplitude)
+
+// const measureKelvin = function (){
+//     const measurement = { 
+//         type: 'temp',
+//         unit: 'celsius',
+//         value: Number(prompt('Degree celsius:')),
+//     }
+//     const kelvin = measurement.value+273;
+//     return kelvin;
+// }
+
+// console.log(measureKelvin());
+
+const maxTemp = [17, 21, 23];
+
